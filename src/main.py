274fd4bf6da415e33cd6671564e0a6441266e59b
Dramelac <dramelac@users.noreply.github.com>
2025-12-11 22:55:01 +0100

    Display client MCP configuration template

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.assets import *
+from src.mcp_app import mcp_host, mcp_port
 
 __version__ = "0.0.1a1"
-
 
 
 def main():
@@ -11,6 +11,13 @@ def main():
     #  - add tools for orchestrator
     #  - add tools for container
 
+    print("MCP client template:")
+    print("========================================================")
+    print(f""""exegol-mcp": {{
+    "type": "http",
+    "url": "http://{mcp_host}:{mcp_port}/mcp"
+}}""")
+    print("========================================================")
     mcp_server.run(transport="streamable-http")
 
 
--- src/mcp_app.py
+++ src/mcp_app.py
@@ -1,3 +1,11 @@
 from mcp.server.fastmcp import FastMCP
 
-mcp_server = FastMCP("Exegol", stateless_http=True, json_response=True)
+mcp_host = "127.0.0.1"
+mcp_port = 8000
+mcp_server = FastMCP(
+    "Exegol",
+    host=mcp_host,
+    port=mcp_port,
+    stateless_http=True,
+    json_response=True
+)
