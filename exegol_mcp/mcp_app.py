f74723494078b3300b914be95edc437187e8ed92
Dramelac <dramelac@users.noreply.github.com>
2025-12-21 17:10:30 +0100

    Add icon to mcp server (if supported by client)

--- exegol_mcp/mcp_app.py
+++ exegol_mcp/mcp_app.py
@@ -1,4 +1,5 @@
 from mcp.server.fastmcp import FastMCP
+from mcp.types import Icon
 
 mcp_host = "127.0.0.1"
 mcp_port = 8000
@@ -8,5 +9,9 @@ mcp_server = FastMCP(
     port=mcp_port,
     stateless_http=False,
     json_response=True,
-    website_url="https://exegol.com"
+    website_url="https://exegol.com",
+    icons=[Icon(
+        src="https://docs.exegol.com/images/Exegol_Symbol_DarkVersion.svg",
+        mimeType="image/svg+xml"
+    )]
 )
